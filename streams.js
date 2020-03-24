cf178391d654627c175108ff1c9d79a5135802ef
Mridul Singh <msmridulsingh@gmail.com>
2020-03-24 17:38:57 +0530

    Add files via upload
    Working with streams

---
+++ streams.js
@@ -0,0 +1,9 @@
+//Working with readable and writable streams
+
+const fs = require('fs');
+const readStream = fs.createReadStream('./example.txt', 'utf-8');
+const writeStream = fs.createWriteStream('example2.txt');
+
+readStream.on('data', (chunk) => {
+    writeStream.write(chunk);
+});
