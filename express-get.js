81cbe6f2ef49dbbf5dc53e31ecbadc3b280029bf
Mridul Singh <msmridulsingh@gmail.com>
2020-03-24 21:38:44 +0530

    Add files via upload

---
+++ express-get.js
@@ -0,0 +1,20 @@
+//Working with Express Http Get Request, Route Params and Query Strings
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/example', (req, res) => {
+    res.send('Hitting example route');
+});
+
+app.get('/example/:name/:age', (req, res) => {
+     console.log(req.params);
+     console.log(req.query);
+
+     res.send(req.params.name + " : " + req.params.age);
+});
+
+app.listen(3000);
